0b77ee8ea4c5e843013454abf23c1b539116838c
cynthia iloekwe <cyniloekwe@gmail.com>
2019-08-30 13:16:20 +0100

    added view router plugin

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,9 @@
 <template>
-  <v-app>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data: () => ({
-    //
-  }),
-};
-</script>
