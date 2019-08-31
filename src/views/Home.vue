80dbf7b7c589f1948585090948804cab1a89edba
cynthia iloekwe <cyniloekwe@gmail.com>
2019-08-31 04:08:36 +0100

    working route

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
   <div class='home'>
     <h1>Homepage</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed tempora cum incidunt dolore qui voluptatibus autem labore modi magnam earum.</p>
+    <!-- to get more of this class format, search for color on the vue site -->
+    <p class='teal darken-4 indigo--text' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed tempora cum incidunt dolore qui voluptatibus autem labore modi magnam earum.</p>
+    <p class='pink lighten-4 black--text text--darken-4' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed tempora cum incidunt dolore qui voluptatibus autem labore modi magnam earum.</p>
+    <!-- for more text size, weight classes and so much more props on text, search for font size page on the vue site -->
+<h1 class='display-4 font-weight-bold'> Massive Display</h1>
+<h1 class='display-3'> Smaller Display</h1>
+<h1 class='display-2'> Smaller Display</h1>
+<h1 class='display-1'> Smaller Display</h1>
+<h1 class='headline'> This is a headline</h1>
+
+
+
+
     </div>
 </template>
 
@@ -9,6 +21,6 @@
 
 
 export default {
-  
+  name: 'Home'
 };
 </script>
