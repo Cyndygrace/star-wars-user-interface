048b5d4015e2a856a76bde4214b3f859017c05ee
cynthia iloekwe <cyniloekwe@gmail.com>
2019-09-01 08:23:41 +0100

    added logo and comments to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
+    <!-- container -->
     <v-card hover flat height="500" class="mx-auto">
+      <!-- image background -->
       <v-img class="white--text" height="640px" src="../assets/image/hero-banner.jpg">
-
+      <!-- logo -->
+        <v-img
+          class="white--text ml-12 mt-8"
+          height="50px"
+          max-width="120"
+          src="../assets/image/logo.png"
+        ></v-img>
       </v-img>
     </v-card>
   </div>
