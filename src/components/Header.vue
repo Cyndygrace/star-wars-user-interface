bd053087a3f7f8f80eb211b169617fdce16a0b08
cynthia iloekwe <cyniloekwe@gmail.com>
2019-09-01 08:20:55 +0100

    added background image for header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <v-card hover flat height="500" class="mx-auto"></v-card>
+    <v-card hover flat height="500" class="mx-auto">
+      <v-img class="white--text" height="640px" src="../assets/image/hero-banner.jpg">
+
+      </v-img>
+    </v-card>
   </div>
 </template>
 
