f05a9addf4f6385dcaa2ccacdf2ee5ad4058d175
cynthia iloekwe <cyniloekwe@gmail.com>
2019-09-02 13:39:55 +0100

    first design for planet card

---
+++ src/components/PlanetCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card
+    class=""
+    max-width="350"
+  >
+    <v-img
+      class="white--text"
+      height="400px"
+      src="../assets/image/planet-1.jpg"
+    >
+      <v-card-title class="align-end fill-height">Top 10 Australian beaches</v-card-title>
+    </v-img>
+
+  </v-card>
+</template>
+
+<script>
+export default {
+  name:'PlanetCard'
+}
+</script>>
