407c1dcaf24c3c492f2ac7de30d869a1884cf624
cynthia iloekwe <cyniloekwe@gmail.com>
2019-09-03 03:50:30 +0100

    working on responsiveness

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -3,14 +3,14 @@
     <div v-bind:key="character.id" v-for="character in characters">
       <v-row class="mx-auto ml-12">
 
-        <v-card hover class="py-0 mb-12" max-height='390' max-width="600">
+        <v-card hover class="py-0 mb-12" max-height='390' min-width="600" max-width='600'>
           
             <v-row>
-              <v-img class="my-0 " width="100" height='390' src="../assets/image/character-1.jpg"></v-img>
+              <v-img class="my-0 " max-width="350" height='390' src="../assets/image/character-1.jpg"></v-img>
             
             <v-col>
               <v-card-text class="pb-0">
-                <p class="title font-weight-bold text--primary mb-0">Name: <br>{{character.name}}</p>
+                <p class="title font-weight-bold text--primary mb-0">Name: {{character.name}}</p>
                 <div class="mb-4">Year: {{character.birth_year}}</div>
 
                 <div class="text--primary">Gender: {{character.gender}}</div>
