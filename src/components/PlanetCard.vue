914b9cdc1d504b67a015c4aac2d7e3100d0e7c33
cynthia iloekwe <cyniloekwe@gmail.com>
2019-09-06 22:12:52 +0100

    added code to dynamically populate data

--- src/components/PlanetCard.vue
+++ src/components/PlanetCard.vue
@@ -1,25 +1,58 @@
 <template>
+<v-container class="ma-auto" fluid>
+  <v-row :align="alignment" :justify="justify" class="grey lighten-5">
   <v-card
-  elevation
   hover
-    class=""
-    max-width="350"
+  tile
+  elevation
+  v-for="(planet, index) in planets"
+  v-bind:key="index"
+  v-if="index<=2"
+  hover
+    class="ma-1 pa-0 ma-8"
+    outlined
+    title
+    max-width="390"
   >
     <v-img
     elevation
     hover
       class="white--text"
       height="400px"
-      src="../assets/image/planet-1.jpg"
+      :src="`../assets/image/planet-${Math.ceil(Math.random()*3)}.jpg`"
     >
-      <v-card-title class="align-end fill-height">Top 10 Australian beaches</v-card-title>
+    <div class="mt-12 align-end fill-height">
+      <v-card-title class="align-end my-12 mb-0 align-sm-content-end align-content-end" font-weight-bold>Name: {{planet.name}}</v-card-title><br />
+      <span class="py-0 title align-end fill-height" font-weight-bold>Climate: {{planet.climate}}</span>
+              <br />
+              <span class="py-0 title align-end fill-height" font-weight-bold>Population: {{planet.population}}</span>
+              <br />
+</div>
     </v-img>
 
   </v-card>
+  </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name:'PlanetCard'
+  name:'PlanetCard',
+  props: ["planets"],
+  data() {
+    return {
+      alignmentsAvailable: ["start", "center", "end", "baseline", "stretch"],
+      alignment: "center",
+      dense: false,
+      justifyAvailable: [
+        "start",
+        "center",
+        "end",
+        "space-around",
+        "space-between"
+      ],
+      justify: "center"
+    }
+  }
 }
-</script>>
+</script>
