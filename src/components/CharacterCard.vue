93cc16422266cafb21d998067d3a60b49ceb921f
cynthia iloekwe <cyniloekwe@gmail.com>
2019-09-06 22:11:37 +0100

    added container fluid to position card

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -1,12 +1,24 @@
 <template>
-  <div>
-    <div v-bind:key="character.id" v-for="character in characters">
-      <v-row class="mx-auto ml-12">
+<v-container class="ma-12 ml-12 mb-0" fluid>
+  <v-row :align="alignment" :jsutify="justify" class="grey lighten-5">
+  
+    <!-- <div v-bind:key="character.id" v-for="character in characters">
+      <v-row class="mx-auto ml-12"> -->
 
-        <v-card hover class="py-0 mb-12" max-height='390' min-width="600" max-width='600'>
+        <v-card 
+        hover 
+        tile
+        class="ma-1 py-0 ma-8 pa-0 " 
+        max-height='390' min-width="700" max-width='700'
+        v-for="(character, index) in characters"
+        v-bind:key="index"
+        v-if="index <= 3"
+        outlined
+        title
+        >
           
             <v-row>
-              <v-img class="my-0 " max-width="350" height='390' src="../assets/image/character-1.jpg"></v-img>
+              <v-img class="my-0 " max-width="400" height='390' :src="`../assets/image/character-${Math.ceil(Math.random()*4)}.jpg`"></v-img>
             
             <v-col>
               <v-card-text class="pb-0">
@@ -16,10 +28,10 @@
                 <div class="text--primary">Gender: {{character.gender}}</div>
               </v-card-text>
               <v-card-actions class="pt-0 pb-0">
-                <v-btn text small class=" black--text">Read More</v-btn>
+                <v-btn text small class=" white lighten-5">Read More</v-btn>
               </v-card-actions>
               <v-divider
-                class=" ml-4 pt-1 black mt-0"
+                class=" ml-2 pt-1 black mt-0"
                 style="opacity: 4"
                 role="seperator"
                 width="100"
@@ -28,14 +40,30 @@
             </v-col>
           </v-row>
         </v-card>
-      </v-row>
-    </div>
-  </div>
+      <!-- </v-row> -->
+    <!-- </div> -->
+  
+  </v-row>
+</v-container>
 </template>
 
 <script>
 export default {
   name: "CharacterCard",
-  props: ["characters"]
+  props: ["characters"],
+  data() {
+    return {
+alignmentsAvailable:["start", "center", "end", "baseline", "stretch"],
+alignment: "center",
+dense: false,
+jsutifyAvailable: [
+  "start",
+  "center",
+  "end",
+  "space-around",
+  "space-between"
+]
+    }
+  }
 };
 </script>>
