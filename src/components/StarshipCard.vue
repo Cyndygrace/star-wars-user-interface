a9e8d3e676641fa37c39583e753ae4448559fd30
cynthia iloekwe <cyniloekwe@gmail.com>
2019-09-06 22:19:53 +0100

    conditional rendering

--- src/components/StarshipCard.vue
+++ src/components/StarshipCard.vue
@@ -1,57 +1,69 @@
 <template>
-  <div>
-    <div v-bind:key="starship.name" v-for="starship in starships">
-      <v-row class="mx-auto ml-12">
-        <!-- <v-col> -->
-        <v-container fluid>
-          <v-row
-            :align="alignment"
-            :justify="justify"
-            class="grey lighten-5"
-            style="height: 300px;"
-          >
-            <v-card hover 
-            
-            max-width="350" 
-            v-for='n in 3'
-            :key='n'
-            class='ma-3 pa-6'
-            outlined
-            title>
-              <v-img class="white--text" height="200px" src="../assets/image/starship-1.jpg"></v-img>
-
-              <v-card-text class="justify-content">
-                <v-card-title
-                  class="title font-weight-bold text--primary pt-0 pb-0 pl-0"
-                >Name: {{starship.name}}</v-card-title>
-                <br />
-                <span class="py-0 subtitle-1">Model: {{starship.starship_class}}</span>
-                <br />
-                <span class="py-0 subtitle-1">Capacity: {{starship.cargo_capacity}}</span>
-                <br />
-              </v-card-text>
-
+  <v-container class="ma-auto" fluid>
+    <v-row :align="alignment" :justify="justify" class="grey lighten-5">
+      <v-card
+        hover
+        tile
+        max-width="390"
+        v-for="(starship, index) in starships"
+        v-bind:key="index"
+        v-if="index<=5"
+        class="ma-1 pa-0 ma-8"
+        outlined
+        title
+      >
+        <v-img class="white--text" height="200px" :src="`../assets/image/starship-${Math.ceil(Math.random()*6)}.jpg`"></v-img>
+        <div class="pb-2 grey lighten-1">
+          <div class="grey lighten-2 ml-2 mr-n2">
+            <v-card-text class="justify-content">
+              <v-card-title
+                class="title font-weight-bold text--primary pt-0 pb-0 pl-0"
+              >Name: {{starship.name}}</v-card-title>
+              <br />
+              <span class="py-0 subtitle-1">Model: {{starship.starship_class}}</span>
+              <br />
+              <span class="py-0 subtitle-1">Capacity: {{starship.cargo_capacity}}</span>
+              <br />
+            </v-card-text>
+            <div class="d-flex justify-end pr-3 pb-1">
               <v-card-actions>
                 <div class="text-right pl-0">
                   <div class="flex-grow-1"></div>
-                  <!-- to remove shadow -->
-                  <v-btn depressed dark color="black">Read More</v-btn>
+                  <v-btn depressed large tile class="grey lighten-1 px-7">
+                    <span class="overline">Read more</span>
+                    <v-icon x-small left>mdi-arrow-right</v-icon>
+                  </v-btn>
                 </div>
               </v-card-actions>
-            </v-card>
-          </v-row>
-        </v-container>
-        <!-- </v-col> -->
-      </v-row>
-    </div>
-  </div>
+            </div>
+          </div>
+        </div>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 
 <script>
 export default {
   name: "StarshipCard",
-  props: ["starships"]
+  props: ["starships"],
+  data() {
+    return {
+      alignmentsAvailable: ["start", "center", "end", "baseline", "stretch"],
+      alignment: "center",
+      dense: false,
+      justifyAvailable: [
+        "start",
+        "center",
+        "end",
+        "space-around",
+        "space-between"
+      ],
+      justify: "center"
+    };
+  },
+  
 };
 </script>
 
